00d14fb317a8d677c04cc330b3eb4e097d9f3631
qiuqilin <951686695@qq.com>
2020-05-18 11:10:22 +0800

    save

--- app/src/main/java/com/congda/tianjianxin/ui/activity/MainActivity.kt
+++ app/src/main/java/com/congda/tianjianxin/ui/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.congda.tianjianxin.ui.activity
 
 import android.view.View
 import android.widget.CheckedTextView
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
@@ -9,7 +10,10 @@ import com.congda.baselibrary.base.BaseActivity
 import com.congda.baselibrary.utils.IMStatusBarUtil
 import com.congda.tianjianxin.R
 import com.congda.tianjianxin.adapter.MyViewPagerAdapter
-import com.congda.tianjianxin.ui.fragment.mvp.ui.*
+import com.congda.tianjianxin.ui.fragment.mvp.ui.FirstFragment
+import com.congda.tianjianxin.ui.fragment.mvp.ui.FourthFragment
+import com.congda.tianjianxin.ui.fragment.mvp.ui.ListFirstFragment
+import com.congda.tianjianxin.ui.fragment.mvp.ui.ThirdeFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity(), ViewPager.OnPageChangeListener, View.OnClickListener {
@@ -17,7 +21,7 @@ class MainActivity : BaseActivity(), ViewPager.OnPageChangeListener, View.OnClic
     var mLastIndex = 0
     var mFragments: MutableList<Fragment> = mutableListOf()
     lateinit var mAdapter: MyViewPagerAdapter
-    lateinit var mCheckedTextViews: Array<CheckedTextView?>
+    lateinit var mLinearLayout: Array<LinearLayout?>
 
     override fun getLayoutId(): Int {
         return R.layout.activity_main
@@ -48,10 +52,11 @@ class MainActivity : BaseActivity(), ViewPager.OnPageChangeListener, View.OnClic
     }
 
     private fun initTabs() {
-        mCheckedTextViews = arrayOfNulls<CheckedTextView>(tabs.childCount)
+        setCheckPosition(0)
+        mLinearLayout = arrayOfNulls<LinearLayout>(tabs.childCount)
         for (i in 0 until tabs.childCount) {
-            mCheckedTextViews[i] = tabs.getChildAt(i) as CheckedTextView
-            mCheckedTextViews[i]?.setOnClickListener(this)
+            mLinearLayout[i] = tabs.getChildAt(i) as LinearLayout
+            mLinearLayout[i]?.setOnClickListener(this)
         }
     }
 
@@ -65,20 +70,22 @@ class MainActivity : BaseActivity(), ViewPager.OnPageChangeListener, View.OnClic
         if (position == mLastIndex) {
             return
         }
-        mCheckedTextViews[position]!!.isChecked = true
-        mCheckedTextViews[mLastIndex]!!.isChecked = false
+        (mLinearLayout[position]!!.getChildAt(1) as CheckedTextView).isChecked = true
+        (mLinearLayout[mLastIndex]!!.getChildAt(1) as CheckedTextView).isChecked = false
+        setCheckPosition(position)
         mLastIndex = position
     }
     override fun onClick(v: View?) {
-        for (i in mCheckedTextViews.indices) {
-            if (v === mCheckedTextViews[i]) {
+        for (i in mLinearLayout.indices) {
+            if (v === mLinearLayout[i]) {
                 if (i == mLastIndex) {
                     break
                 }
-                mCheckedTextViews[i]!!.isChecked = true
-                mCheckedTextViews[mLastIndex]!!.isChecked = false
+                (mLinearLayout[i]!!.getChildAt(1) as CheckedTextView).isChecked = true
+                (mLinearLayout[mLastIndex]!!.getChildAt(1) as CheckedTextView).isChecked = false
                 mLastIndex = i
                 viewpage.setCurrentItem(i, false)
+                setCheckPosition(i)
                 break
             }
         }
@@ -90,6 +97,44 @@ class MainActivity : BaseActivity(), ViewPager.OnPageChangeListener, View.OnClic
             exitTime = System.currentTimeMillis()
         } else {
             finish()
+        }
+    }
+
+
+    /**
+     * 设置选定状态
+     * @param position
+     */
+    private fun setCheckPosition(position: Int) {
+        when (position) {
+            0 -> {
+                iv_tab2.setImageResource(R.mipmap.im_main_contact)
+                iv_tab3.setImageResource(R.mipmap.im_main_find)
+                iv_tab4.setImageResource(R.mipmap.im_main_mine)
+                iv_tab1.setAnimation("huihuajson.json")
+                iv_tab1.playAnimation()
+            }
+            1 -> {
+                iv_tab1.setImageResource(R.mipmap.im_main_conver)
+                iv_tab3.setImageResource(R.mipmap.im_main_find)
+                iv_tab4.setImageResource(R.mipmap.im_main_mine)
+                iv_tab2.setAnimation("txunl.json")
+                iv_tab2.playAnimation()
+            }
+            2 -> {
+                iv_tab1.setImageResource(R.mipmap.im_main_conver)
+                iv_tab2.setImageResource(R.mipmap.im_main_contact)
+                iv_tab4.setImageResource(R.mipmap.im_main_mine)
+                iv_tab3.setAnimation("faxian.json")
+                iv_tab3.playAnimation()
+            }
+            3 -> {
+                iv_tab1.setImageResource(R.mipmap.im_main_conver)
+                iv_tab2.setImageResource(R.mipmap.im_main_contact)
+                iv_tab3.setImageResource(R.mipmap.im_main_find)
+                iv_tab4.setAnimation("mine.json")
+                iv_tab4.playAnimation()
+            }
         }
     }
 }
